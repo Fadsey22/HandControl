7891d1e0b2dc154e510b877d5261ade52a9b4a83
Fadsey22 <81942477+Fadsey22@users.noreply.github.com>
2021-10-03 01:15:44 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ import os
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from mediapipe.python.solutions import holistic
 
+def calc_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
 
 def mpsu():
     mp_d = mp.solutions.drawing_utils
@@ -41,7 +53,7 @@ def detect():
             ret, frame = cap.read()  # read feed from webcam
             pic = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             val = holistic.process(pic)
-            mp_drawing.draw_landmarks(pic, val.face_landmarks, mp_holistic.FACE_CONNECTIONS) # draws everything face related
+            #mp_drawing.draw_landmarks(pic, val.face_landmarks, mp_holistic.FACE_CONNECTIONS) # draws everything face related
 
             # Right hand
            # mp_drawing.draw_landmarks(pic, val.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # rhand related
@@ -52,19 +64,26 @@ def detect():
             mp_drawing.draw_landmarks(pic, val.pose_landmarks, mp_holistic.POSE_CONNECTIONS)  # should related
             landmarks = val.pose_landmarks.landmark
             try:
+                LWrist = [landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].x,
+                          landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].y]
+                LElbow = [landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].x,
+                          landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y]
+                LShoulder = [landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].x,
+                          landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].y]
+                ang1 = calc_angle(LShoulder, LElbow, LWrist)
+                cv2.putText(pic, str(ang1), (80, 475), cv2.FONT_HERSHEY_COMPLEX, 2, (200, 0, 0), 10)
                 #print(landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y, "," , landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].x)
-                if ft != 0:
-                    print(abs(landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y - prevLElbowY))
-                    if t.time() - Ljabtimer >= 0.5 and abs(landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y - prevLElbowY) >= 0.05:
-                        Ljabtimer = t.time()
-                        retract = True
-                        cv2.putText(pic, "JAB", (80, 375), cv2.FONT_HERSHEY_COMPLEX, 4, (200, 0, 0), 10)
-                    if t.time() - Rcrosstimer >= 0.5 and abs(landmarks[mp_holistic.PoseLandmark.RIGHT_ELBOW.value].y - prevRElbowY) >= 0.05:
-                        Rcrosstimer = t.time()
-                        cv2.putText(pic, "CROSS", (80, 475), cv2.FONT_HERSHEY_COMPLEX, 4, (200, 0, 0), 10)
-
-                prevLElbowY = landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y
-                prevRElbowY = landmarks[mp_holistic.PoseLandmark.RIGHT_ELBOW.value].y
+                # if ft != 0:
+                #     if t.time() - Ljabtimer >= 0.5 and abs(landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y - prevLElbowY) >= 0.05:
+                #         Ljabtimer = t.time()
+                #         retract = True
+                #         cv2.putText(pic, "JAB", (80, 375), cv2.FONT_HERSHEY_COMPLEX, 4, (200, 0, 0), 10)
+                #     if t.time() - Rcrosstimer >= 0.5 and abs(landmarks[mp_holistic.PoseLandmark.RIGHT_ELBOW.value].y - prevRElbowY) >= 0.05:
+                #         Rcrosstimer = t.time()
+                #         cv2.putText(pic, "CROSS", (80, 475), cv2.FONT_HERSHEY_COMPLEX, 4, (200, 0, 0), 10)
+                #
+                # prevLElbowY = landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y
+                # prevRElbowY = landmarks[mp_holistic.PoseLandmark.RIGHT_ELBOW.value].y
                 ft = 1
                 # LeftArm.append(landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].x)
                 # LeftArm.append(landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].y)
